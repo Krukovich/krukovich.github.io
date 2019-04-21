5e580264d35e47eddc82d91a93e2b8eb5a8d0a3d
Krukovich <44588100+Krukovich@users.noreply.github.com>
2019-04-21 21:50:16 +0300

    add

--- vk_js_app/src/index.js
+++ vk_js_app/src/index.js
@@ -1,13 +1,11 @@
 'use strict'
 
 import main from "../css/main.css";
-import controller from "../src/controller/controller"
-import showUserPhotos from "../src/controller/controllerPhotos"
-import login from "../src/controller/controllerLogin"
+import controller from "../src/controller/controller";
+import showUserPhotos from "../src/controller/controllerPhotos";
 
 controller();
 showUserPhotos();
-login();
 
 
 
